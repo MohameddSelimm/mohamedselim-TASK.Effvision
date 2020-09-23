6824fb4ff63359f059107768e424823fe92e9ff2
Mohamed Selim <MohameddSelimm@gmail.com>
2020-09-23 16:56:48 +0200

    initial commit

---
+++ js/main.js
@@ -0,0 +1,150 @@
+$("#muscles").click(function () {
+  $(".do").removeClass("red-body");
+  $(".dd").removeClass("red-text");
+  $("#muscles2").addClass("red-body");
+  $("#text9").addClass("red-text");
+});
+
+$("#kidneys").click(function () {
+  $(".do").removeClass("red-body");
+  $(".dd").removeClass("red-text");
+  $("#kidneys2").addClass("red-body");
+  $("#text12").addClass("red-text");
+});
+
+$("#Retina").click(function () {
+  $(".do").removeClass("red-body");
+  $(".dd").removeClass("red-text");
+  $("#Retina2").addClass("red-body");
+  $("#text10").addClass("red-text");
+});
+$("#joints2").click(function () {
+  $(".do").removeClass("red-body");
+  $(".dd").removeClass("red-text");
+  $("#joints2").addClass("red-body");
+  $("#text3").addClass("red-text");
+});
+$("#Peripheral-Nervves").click(function () {
+  $(".do").removeClass("red-body");
+  $(".dd").removeClass("red-text");
+  $("#nervs2").addClass("red-body");
+  $("#text4").addClass("red-text");
+});
+$("#blood-vessels").click(function () {
+  $(".do").removeClass("red-body");
+  $(".dd").removeClass("red-text");
+  $("#blood-vessels2").addClass("red-body");
+  $("#text7").addClass("red-text");
+});
+$("#bone-marrow").click(function () {
+  $(".do").removeClass("red-body");
+  $(".dd").removeClass("red-text");
+  $("#bone-marrow2").addClass("red-body");
+  $("#text8").addClass("red-text");
+});
+$("#heart").click(function () {
+  $(".do").removeClass("red-body");
+  $(".dd").removeClass("red-text");
+  $("#heart2").addClass("red-body");
+  $("#text6").addClass("red-text");
+});
+$("#thyroid").click(function () {
+  $(".do").removeClass("red-body");
+  $(".dd").removeClass("red-text");
+  $("#thyroid2").addClass("red-body");
+  $("#text11").addClass("red-text");
+});
+$("#skin").click(function () {
+  $(".do").removeClass("red-body");
+  $(".dd").removeClass("red-text");
+  $("#skin2").addClass("red-body");
+  $("#text1").addClass("red-text");
+});
+$("#bones").click(function () {
+  $(".do").removeClass("red-body");
+  $(".dd").removeClass("red-text");
+  $("#bones2").addClass("red-body");
+  $("#text2").addClass("red-text");
+});
+$("#teeth").click(function () {
+  $(".do").removeClass("red-body");
+  $(".dd").removeClass("red-text");
+  $("#teeth2").addClass("red-body");
+  $("#text5").addClass("red-text");
+});
+
+//////////////////////////////////////////////
+
+
+$("#text1").click(function () {
+    $(".do").removeClass("red-body");
+    $(".dd").removeClass("red-text");
+    $("#skin2").addClass("red-body");
+    $("#text1").addClass("red-text");
+  });
+  $("#text2").click(function () {
+    $(".do").removeClass("red-body");
+    $(".dd").removeClass("red-text");
+    $("#bones2").addClass("red-body");
+    $("#text2").addClass("red-text");
+  });
+  $("#text3").click(function () {
+    $(".do").removeClass("red-body");
+    $(".dd").removeClass("red-text");
+    $("#joints2").addClass("red-body");
+    $("#text3").addClass("red-text");
+  });
+  $("#text4").click(function () {
+    $(".do").removeClass("red-body");
+    $(".dd").removeClass("red-text");
+    $("#nervs2").addClass("red-body");
+    $("#text4").addClass("red-text");
+  });
+  $("#text5").click(function () {
+    $(".do").removeClass("red-body");
+    $(".dd").removeClass("red-text");
+    $("#teeth2").addClass("red-body");
+    $("#text5").addClass("red-text");
+  });
+  $("#text6").click(function () {
+    $(".do").removeClass("red-body");
+    $(".dd").removeClass("red-text");
+    $("#heart2").addClass("red-body");
+    $("#text6").addClass("red-text");
+  });
+  $("#text7").click(function () {
+    $(".do").removeClass("red-body");
+    $(".dd").removeClass("red-text");
+    $("#blood-vessels2").addClass("red-body");
+    $("#text7").addClass("red-text");
+  });
+  $("#text8").click(function () {
+    $(".do").removeClass("red-body");
+    $(".dd").removeClass("red-text");
+    $("#bone-marrow2").addClass("red-body");
+    $("#text8").addClass("red-text");
+  });
+  $("#text9").click(function () {
+    $(".do").removeClass("red-body");
+    $(".dd").removeClass("red-text");
+    $("#muscles2").addClass("red-body");
+    $("#text9").addClass("red-text");
+  });
+  $("#text10").click(function () {
+    $(".do").removeClass("red-body");
+    $(".dd").removeClass("red-text");
+    $("#Retina2").addClass("red-body");
+    $("#text10").addClass("red-text");
+  });
+  $("#text11").click(function () {
+    $(".do").removeClass("red-body");
+    $(".dd").removeClass("red-text");
+    $("#thyroid2").addClass("red-body");
+    $("#text11").addClass("red-text");
+  });
+  $("#text12").click(function () {
+    $(".do").removeClass("red-body");
+    $(".dd").removeClass("red-text");
+    $("#kidneys2").addClass("red-body");
+    $("#text12").addClass("red-text");
+  });
